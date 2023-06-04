9ecaf95eb11cad3aa803c2e44932c73b416755d6
Lautaro <altairdiaz245@gmail.com>
2023-06-04 09:36:47 -0300

    controlador de formulario creado en ./src/controladores/page_form.js

---
+++ src/controladores/page_form.js
@@ -0,0 +1,22 @@
+import { sql } from "../db.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const DB_DATABASE_NAME = process.env.DB_DATABASE_NAME;
+export const renderForm = (_req, res) => res.render("form");
+
+export const saveInfoInDB = (req, res) => {
+  const { juego, img, created_by } = req.body;
+  const ordenSQL = `insert into ${DB_DATABASE_NAME} (juego, created_by, img) 
+    values 
+      (${sql.escape(juego)}, ${created_by}, ${sql.escape(img)})
+     `
+  sql.query(ordenSQL , (err, _out) => {
+    if (err) console.log(err)
+    console.log("guardado")
+  })
+  res.redirect("/")
+}
+
+  
