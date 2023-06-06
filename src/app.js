f2eac1043fc03ebc3d66e50af74cd2ac8e88a6e7
Lautaro <altairdiaz245@gmail.com>
2023-06-05 22:29:42 -0300

    Controlador para editar un elemento agregado - CRUD listo

--- src/app.js
+++ src/app.js
@@ -3,19 +3,19 @@ import { paginaPrincipal } from "./routers/pagina_principal.js";
 import { __dirname } from "./__dirname.js";
 import { pageNotFound } from "./routers/page_not_found.js";
 import { form } from "./routers/form.js";
+import methodOverride from "method-override";
 
 // Importar y configurar dotenv
 import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import methodOverride from 'method-override'
 const app = express();
 
 // Configuracion de express;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
 
 // Motor de plantillas
 app.set("views", __dirname("views"));
@@ -27,7 +27,7 @@ app.use(express.static(__dirname("public")));
 // paginas
 app.use(paginaPrincipal);
 app.use(form);
-app.use(pageNotFound)
+app.use(pageNotFound);
 
 app.listen(process.env.PORT ?? 8080, () =>
   console.log(`server en http://localhost:${process.env.PORT ?? 8080}`)
--- src/controladores/page_form.js
+++ src/controladores/page_form.js
@@ -1,6 +1,9 @@
 import { sql } from "../db.js";
 import * as dotenv from "dotenv";
 
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
+
 dotenv.config();
 
 const DB_DATABASE_NAME = process.env.DB_DATABASE_NAME;
@@ -8,12 +11,14 @@ export const renderForm = (_req, res) => res.render("form");
 
 export const saveInfoInDB = (req, res) => {
   const { juego, img, created_by } = req.body;
-  const ordenSQL = `insert into ${DB_DATABASE_NAME} (juego, created_by, img) 
+  const ordenSQL = `insert into ${DB_DATABASE_NAME} (game, created_by, image, uuid_FRONT_END) 
     values 
-      (${sql.escape(juego)}, ${created_by}, ${sql.escape(img)})
-     `
-  sql.query(ordenSQL , (err, out) => err ? console.log(err) : console.log("guardado", out))
-  res.redirect("/")
-}
-
-  
+      (${sql.escape(juego)}, ${created_by}, ${sql.escape(img)}, ${sql.escape(
+    uuidv4()
+  )})
+     `;
+  sql.query(ordenSQL, (err, out) =>
+    err ? console.log(err) : console.log("guardado")
+  );
+  res.redirect("/");
+};
