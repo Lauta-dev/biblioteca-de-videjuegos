838973ce02f2778451138d1271eaacf9f602b7a2
Lautaro <altairdiaz245@gmail.com>
2023-06-13 17:31:19 -0300

    webapp config añadidio

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const webpack = require("webpack");
+require("dotenv").config();
+
+module.exports = {
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  node: {
+    global: false,
+    __filename: false,
+    __dirname: false,
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      SUPABASE_API_KEY: toString(process.env.SUPABASE_API_KEY),
+      SUPABASE_URL: toString(process.env.SUPABASE_URL),
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
